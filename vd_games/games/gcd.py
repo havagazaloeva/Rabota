9bd16743f76b7f30aaf8bd98222bfe1c40178a1c
havagazaloeva <havagaloeva@gmail.com>
2026-04-05 22:23:18 +0300

    ПР №6

--- vd_games/games/gcd.py
+++
@@ -1,9 +0,0 @@
-import random
-import math
-
-def generate_question_gcd():
-    a = random.randint(1, 50)
-    b = random.randint(1, 50)
-    correct = math.gcd(a, b)
-    question = f"{a} {b}"
-    return question, correct
